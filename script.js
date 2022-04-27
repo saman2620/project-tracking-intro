0856e5543e95b068544257762a1a4bbb20acb1ca
saman2620 <102049962+saman2620@users.noreply.github.com>
2022-04-27 21:21:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+const { body } = document;
+
+
+hamburger.addEventListener('click', e => {
+   
+    body.classList.toggle('show-nav');
+   
+});
